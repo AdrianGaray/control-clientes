55bea3fbe05b5df972baeda123ad6339bad25630
AdrianGaray <adriangaray396@gmail.com>
2026-03-01 14:02:58 -0300

    Capítulo 99 - Configuración Módulo Firestore y Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAlipmedyh4HM9ObKvUj_BtMwF8X5LiYHo",
+        authDomain: "control-clientes-c3d38.firebaseapp.com",
+        projectId: "control-clientes-c3d38",
+        storageBucket: "control-clientes-c3d38.firebasestorage.app",
+        messagingSenderId: "680048144062",
+        appId: "1:680048144062:web:c1e18bb306f00c2d2b38ef"
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,32 @@
+// se agregar la configuración de Firestore y conectarnos a la base de datos.
+// También se agrega el proveedor de HTTP que usaremos para realizar peticiones a la base de Cloud Firestore
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from './environments/environment.development';
 
-bootstrapApplication(AppComponent, appConfig)
+// Firebase imports para aplicaciones standalone 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    // Añade el cliente HTTP que usaremos para hacer peticiones a Firebase y otras APIs
+    provideHttpClient(),
+    // Inicializa la configuración de Firebase con los valores del archivo environment
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),  // Inicializa Firebase 
+    // Proveedor de Firestore que permite la conexión con la base de datos.
+    provideFirestore(() => getFirestore()), // Inicializa Firestore. 
+    // Proveedor de autenticación para manejar usuarios y sesiones. 
+    provideAuth(() => getAuth()),  // Inicializa Auth 
+    //  Proveedor de almacenamiento para trabajar con archivos en Firebase. 
+    provideStorage(() => getStorage()), // Inicializa Storage 
+    ...appConfig.providers // Desestructuramos los providers del appConfig si es necesario 
+  ]
+})
   .catch((err) => console.error(err));
