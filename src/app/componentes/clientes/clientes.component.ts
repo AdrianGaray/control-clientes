ed437b018bf807abba806827fe33af097c73fd01
AdrianGaray <adriangaray396@gmail.com>
2026-03-01 16:41:41 -0300

    Capítulo 102 - Componente de Cliente - parte 2

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Cliente } from '../../modelo/cliente.modelo';
 import { ClienteService } from '../../servicios/cliente.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [CommonModule], // CommonModule para poder utilizar el pipe de currency en la plantilla
+  // CommonModule para poder utilizar el pipe de currency en la plantilla
+  imports: [CommonModule, RouterModule],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
@@ -22,5 +24,21 @@ export class ClientesComponent {
     this.clienteServicio.getClientes().subscribe(clientes => {
       this.clientes = clientes;  // Almacena el listado en la propiedad 'clientes' 
     });
+  }
+
+  // getSaldoTotal(): number {
+  //   let saldoTotal: number = 0;
+  //   if(this.clientes){
+  //     this.clientes.forEach(cliente => {
+  //       if(cliente.saldo !== undefined){
+  //         saldoTotal += cliente.saldo;
+  //       }
+  //     });
+  //   }
+  //   return saldoTotal;
+  // }
+
+  getSaldoTotal(): number {
+    return this.clientes?.reduce((total, cliente) => total + (cliente.saldo ?? 0), 0) ?? 0;
   }
 }
