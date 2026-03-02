178fbf69a6ef1c1daa2ca4c49e081333e82d96de
AdrianGaray <adriangaray396@gmail.com>
2026-03-01 22:16:10 -0300

    Capítulo 107 - Editar Cliente - parte 2

--- src/app/componentes/editar-cliente/editar-cliente.component.ts
+++ src/app/componentes/editar-cliente/editar-cliente.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Cliente } from '../../modelo/cliente.modelo';
 import { ClienteService } from '../../servicios/cliente.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-cliente',
-  imports: [], // Importamos módulos necesarios 
+  imports: [FormsModule, RouterModule], // Importamos módulos necesarios 
   templateUrl: './editar-cliente.component.html',
   styleUrl: './editar-cliente.component.css'
 })
@@ -23,19 +24,19 @@ export class EditarClienteComponent {
     private clienteServicio: ClienteService,
     private router: Router,
     private route: ActivatedRoute
-  ){}
+  ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     // Obtener el 'id' del parámetro de ruta 
     this.id = this.route.snapshot.paramMap.get('id');
 
-     // Verificar si el 'id' no es null antes de buscar el cliente
-    if(this.id){
+    // Verificar si el 'id' no es null antes de buscar el cliente
+    if (this.id) {
       this.clienteServicio.getCliente(this.id).subscribe((cliente: Cliente | null) => {
-        if(cliente){
+        if (cliente) {
           // Asignar el cliente si existe 
           this.cliente = cliente;
-        } else{
+        } else {
           console.log('Cliente no encontrado: ' + this.id);
 
           // Redirigir al inicio si no se encuentra el cliente 
@@ -48,6 +49,14 @@ export class EditarClienteComponent {
       // Redirigir al inicio si no hay ID 
       this.router.navigate(['/']);
     }
+  }
+
+  guardar(_t9: NgForm) {
+    throw new Error('Method not implemented.');
+  }
+
+  eliminar() {
+    throw new Error('Method not implemented.');
   }
 
 }
