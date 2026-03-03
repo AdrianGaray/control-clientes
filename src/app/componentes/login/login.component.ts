5e31e11352093ab66c1f09ce3aac7dcabeb7bec5
AdrianGaray <adriangaray396@gmail.com>
2026-03-02 21:20:50 -0300

    Capítulo 110 - omponente de Login - parte 1

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  // Utilizamos FormsModule para manejar la entrada de datos del formulario
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  // Las variables email y password almacenarán los datos ingresados en el formulario. 
+  email: string | null = null;
+  password: string | null = null;
+
+  constructor(private router: Router) { }
+
+  // El método login() se encargará de la autenticación una vez que se implemente. 
+  login() {
+    throw new Error('Method not implemented.');
+  }
 }
